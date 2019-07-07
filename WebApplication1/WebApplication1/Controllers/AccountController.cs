3ac7cbc1c4ac54aebef6ddc2909a1b8e0c3ff1ef
GhasemNaseri <programmingwebwin@gmail.com>
2019-07-07 15:13:15 +0330

    ddsdsd

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace WebApplication1.Controllers
 
         public AccountController()
         {
+            //inits
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
